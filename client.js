956657cab600cf7d8f76e704053b85f5710f017d
Shashank Singh <62293640+ssrajputtheboss@users.noreply.github.com>
2021-05-15 18:16:05 +0530

    Host added

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const Client = require('pg').Client;
 require('dotenv').config();
 const client = new Client({
+  host:process.env.DBHOST,
   user:process.env.DBUSER,
   password:process.env.DBPASS,
   port: 5432,
--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const Pool = require('pg').Pool;
 require('dotenv').config();
 const pool = new Pool({
+  host:process.env.DBHOST,
   user:process.env.DBUSER,
   password:process.env.DBPASS,
   port: 5432,
